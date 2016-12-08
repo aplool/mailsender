a47436ce3be093321aec429e0c0a1c583fa3afce
longtai.chuang <longtai.chuang@coretronic.com>
2016-12-08 19:54:11 +0800

    [Change] 修改Main 的寫法

--- src/main/java/com/aplool/mail/Main.java
+++ src/main/java/com/aplool/mail/Main.java
@@ -5,51 +5,79 @@ import com.aplool.mail.model.MailHeaderConfig;
 import com.aplool.mail.model.MailHostConfig;
 import com.aplool.mail.model.MailItem;
 import com.aplool.mail.utils.MailAgent;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 import org.apache.commons.mail.EmailConstants;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created by longtai on 2016/12/1.
  */
 public class Main {
-    public Main() {
+    MailHostConfig mailHostConfig;
+    MailHeaderConfig mailHeaderConfig;
+    String mailListFilename;
+    String messageBody;
+    MailAgent mailAgent;
+    EventBus mailBus = new EventBus();
 
+    public Main(String defaultPath) {
+        String configPath = new File(defaultPath + "/mailHost.config").getAbsolutePath();
+        mailHostConfig = new MailHostConfig(configPath);
+        configPath = new File(defaultPath + "/mailHeader.config").getAbsolutePath();
+        mailHeaderConfig = new MailHeaderConfig(configPath);
+        mailListFilename = new File(defaultPath + "/mailToList.txt").getAbsolutePath();
+        mailAgent = new MailAgent(mailHostConfig);
+        mailAgent.setMailHeaderConfig(mailHeaderConfig);
+        mailAgent.setMarcoPath(new File(defaultPath + "/marco").getAbsolutePath());
+        messageBody = new File(defaultPath + "/mailBody.txt").getAbsolutePath();
+        mailBus.register(new EventBusSendMail());
     }
 
-    public static void main(String[] args) {
-        System.out.println("Mail Send Start :");
+    public void start(){
+        sendMailWithEmailList(mailListFilename);
+    }
+    private void sendMailWithEmailList(String fileName){
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 
-        try {
-            String configPath = new File(args[0] + "/mailHost.config").getAbsolutePath();
-            System.out.println(configPath);
-            MailHostConfig mailHostConfig = new MailHostConfig(configPath);
-            configPath = new File(args[0] + "/mailHeader.config").getAbsolutePath();
-            MailHeaderConfig mailHeaderConfig = new MailHeaderConfig(configPath);
-            String messageBody = new File(args[0] + "/mailBody.txt").getAbsolutePath();
-            List<String> mailToList = MailAgent.loadMailToFromFile(new File(args[0] + "/mailToList.txt").getAbsolutePath());
-            for (String mailToEmail : mailToList) {
-                MailAgent mailAgent = new MailAgent(mailHostConfig);
-                mailAgent.setMailHeaderConfig(mailHeaderConfig);
-                mailAgent.setMarcoPath(new File(args[0] + "/marco").getAbsolutePath());
+            stream.forEach((v)->mailBus.post(v));
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class EventBusSendMail {
+        @Subscribe
+        public void recordCustomerChange(String email) {
+            try {
                 MailItem mailItem = new MailItem();
-                MailAddress toEmail = new MailAddress(mailToEmail, mailToEmail);
+                MailAddress toEmail = new MailAddress(email, email);
                 mailItem.addTo(toEmail);
                 mailItem.contentType = EmailConstants.TEXT_HTML;
                 mailItem.message = mailAgent.loadMessageBodyFromFile(messageBody);
-                try {
-                    Boolean sendResult = mailAgent.sendMail(mailItem);
-                    System.out.println("Mail to: " + mailToEmail + ", Message Body: " + mailItem.message + " => " + sendResult.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                Boolean sendResult = mailAgent.sendMail(mailItem);
+                System.out.println("Mail to: " + email + ", Message Body: " + mailItem.message + " => " + sendResult.toString());
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+    }
+q
+    public static void main(String[] args) {
+        System.out.println("Mail Send Start :");
+
+        Main main = new Main(args[0]);
+        main.start();
+
 
     }
 }
